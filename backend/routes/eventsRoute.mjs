6c1c6c462170db201e5fc0d73f3e7cf92166665b
DanielLubiejewski <lubiejewskidaniel@gmail.com>
2025-03-18 23:10:16 +0000

    Added few comments to eventsRoute.mjs for better understanding my code

--- backend/routes/eventsRoute.mjs
+++ backend/routes/eventsRoute.mjs
@@ -1,6 +1,8 @@
-import express from "express";
-import { fetchEvents } from "../controllers/eventsController.mjs";
+import express from "express"; // Import Express
+import { fetchEvents } from "../controllers/eventsController.mjs"; // Import event handler
 
-const router = express.Router();
-router.get("/:location", fetchEvents);
-export default router;
+const router = express.Router(); // Create router
+
+router.get("/:location", fetchEvents); // Route to get events by location
+
+export default router; // Export router
