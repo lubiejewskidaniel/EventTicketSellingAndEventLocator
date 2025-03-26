257a39c6c1493ef690151a1acb6d06d977739adc
DanielLubiejewski <lubiejewskidaniel@gmail.com>
2025-03-26 22:26:21 +0000

    Small not really important change

--- backend/controllers/ticketsController.mjs
+++ backend/controllers/ticketsController.mjs
@@ -7,7 +7,7 @@ export const fetchTickets = (req, res) => {
 	if (tickets.length === 0) {
 		return res
 			.status(404)
-			.json({ message: "No tickets found for this event." });
+			.json({ message: "No tickets found for this particular  event." });
 	}
 
 	res.json(tickets);
